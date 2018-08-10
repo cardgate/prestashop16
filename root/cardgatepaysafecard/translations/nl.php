3a24d2c265939dfff7851c1d4581618eb3e9ccf0
richard <richard@richard-imac2.dbs.dbcorp.nl>
2018-08-10 13:06:00 +0200

    Version 1.6.25 New payment method: Paysafecard.

---
+++ root/cardgatepaysafecard/translations/nl.php
@@ -0,0 +1,9 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{cardgatepaysafecard}prestashop>cardgatepaysafecard_da9c72b9e543135f3f59e3c8ac68ef35'] = 'Betaal met ';
+$_MODULE['<{cardgatepaysafecard}prestashop>cardgatepaysafecard_6539184f2aa0710ce987ee700e16d225'] = 'CardGate Paysafecard';
+$_MODULE['<{cardgatepaysafecard}prestashop>cardgatepaysafecard_84c13358412ffcbe2b8329e8ad1f3042'] = 'Accepteert betalingen met CardGate PayPal';
+$_MODULE['<{cardgatepaysafecard}prestashop>cardgatepaysafecard_69a1a3ad8dd5da6db3c4da838a0cf9c7'] = 'Weet u zeker dat u uw details wilt verwijderen?';
+$_MODULE['<{cardgatepaysafecard}prestashop>cardgatepaysafecard_59ca7aaff7f6bfd9dd776a6add44770f'] = 'De CardGate module is niet gevonden';
