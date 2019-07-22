441c3e26c4a863b73de233eb51486d8435c2e2e9
richard <richard@cardgate.com>
2019-07-22 15:14:17 +0200

    Check for valid ideal issuers. Version bump.

--- modules/cardgate/cardgatepayment.php
+++ modules/cardgate/cardgatepayment.php
@@ -2,7 +2,7 @@
 
 class CardgatePayment extends PaymentModule {
 
-    var $version = '1.6.30';
+    var $version = '1.6.31';
     var $tab = 'payments_gateways';
     var $author = 'CardGate';
     var $shop_version = _PS_VERSION_;
