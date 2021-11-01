9d6a95401d79e89cbc5a83c1f8c755cd2030c234
richard <richard@cardgate.com>
2021-11-01 16:16:08 +0100

    Version bump.

--- modules/cardgate/cardgate.php
+++ modules/cardgate/cardgate.php
@@ -12,7 +12,7 @@ class Cardgate extends PaymentModule {
         $this->paymentcode = 'cardgate';
         $this->paymentname = 'CardGate';
         $this->tab = 'payments_gateways';
-        $this->version = '1.6.35';
+        $this->version = '1.6.36';
         $this->author = 'CardGate';
         $this->bootstrap = true;
         $this->currencies = true;
